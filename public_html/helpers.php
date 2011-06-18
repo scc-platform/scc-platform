f16b297950f5965c3025e43188e9803c7383ca63
Neill Russell <git@enru.co.uk>
2011-06-18 21:29:37 +0100

    added a "sorry, no helpers" msg to helpers list + a wee bug fix

--- public_html/helpers.php
+++ public_html/helpers.php
@@ -4,11 +4,13 @@ checkUserSession();
 mustBeLoggedIn();
 
 $c = new UserController();
+$s = getSmarty();
 
 // 'user' is the carer they want to 'help'
 if (isset($_GET['action']) && $_GET['action'] == 'help' && 
 	isset($_GET['userID']) && ctype_digit($_GET['userID'])) {
 	$c->associate($_SESSION['userID'], $_GET['userID']);
+	$s->assign('associated', true);
 }
 elseif (isset($_POST['action']) && $_POST['action'] == 'approve' && 
 	isset($_POST['userID']) && ctype_digit($_POST['userID'])) {
@@ -19,13 +21,12 @@ elseif (isset($_POST['action']) && $_POST['action'] == 'approve' &&
 elseif (isset($_POST['action']) && $_POST['action'] == 'ignore' && 
 	isset($_POST['userID']) && ctype_digit($_POST['userID'])) {
 	$c->ignore($_POST['userID']);
-	header("Location: /helpers..php");
+	header("Location: /helpers.php");
 	die();
 }
 
 $helpers = $c->helpers();
 
-$s = getSmarty();
 $s->assign('helpers', $helpers);
 $s->display('helpers.htm');
 
