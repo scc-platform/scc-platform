557f66f26737822ab694ad92132c488b21d5af41
Neill Russell <git@enru.co.uk>
2011-06-18 21:05:04 +0100

    moved links to helpers

---
+++ public_html/helpers.php
@@ -0,0 +1,32 @@
+<?php
+require '../src/global.php';
+checkUserSession();
+mustBeLoggedIn();
+
+$c = new UserController();
+
+// 'user' is the carer they want to 'help'
+if (isset($_GET['action']) && $_GET['action'] == 'help' && 
+	isset($_GET['userID']) && ctype_digit($_GET['userID'])) {
+	$c->associate($_SESSION['userID'], $_GET['userID']);
+}
+elseif (isset($_POST['action']) && $_POST['action'] == 'approve' && 
+	isset($_POST['userID']) && ctype_digit($_POST['userID'])) {
+	$c->approve($_POST['userID']);
+	header("Location: /helpers.php");
+	die();
+}
+elseif (isset($_POST['action']) && $_POST['action'] == 'ignore' && 
+	isset($_POST['userID']) && ctype_digit($_POST['userID'])) {
+	$c->ignore($_POST['userID']);
+	header("Location: /helpers..php");
+	die();
+}
+
+$helpers = $c->helpers();
+
+$s = getSmarty();
+$s->assign('helpers', $helpers);
+$s->display('helpers.htm');
+
+
