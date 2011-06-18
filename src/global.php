c34d0c4c2e2705a7920535dbb43228f6fe2bebc1
James <james@jarofgreen.co.uk>
2011-06-18 14:00:47 +0100

    basic setup:  config options  some helper functions  place to put templates and complied templates

---
+++ src/global.php
@@ -0,0 +1,37 @@
+<?php
+require dirname(__FILE__).'/../config.php';
+
+
+function my_autoload($class_name){
+	$inc = dirname(__FILE__);
+	if(file_exists($inc."/".$class_name.'.class.php')){
+		require_once($inc."/".$class_name.'.class.php');
+		return;
+	}
+}
+spl_autoload_register("my_autoload");
+
+$DB_CONNECTION = null;
+/** @return PDO **/
+function getDB() {
+	global $DB_CONNECTION;
+	if (!$DB_CONNECTION) {
+		$DB_CONNECTION = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+		$DB_CONNECTION->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		$DB_CONNECTION->exec("SET CLIENT_ENCODING TO 'utf8'");
+		$DB_CONNECTION->exec("SET NAMES 'utf8'");
+	}
+	return $DB_CONNECTION;
+}
+
+
+/** @return Smarty **/
+function getSmarty() {
+	global $CURRENT_USER;
+	require_once dirname(__FILE__).'/../smarty/Smarty.class.php';
+	$s = new Smarty();
+	$s->template_dir = dirname(__FILE__) . '/../templates/';
+	$s->compile_dir = dirname(__FILE__) . '/../smarty_c/';
+	return $s;
+}
+
